31cb87d0d56e5119aad70887e8d90b09586b2033
jinwooklim <aiden.limo@outlook.com>
2020-06-02 15:37:14 +0900

    Update test file to unit-test file

--- app/test/functional_test.py
+++ app/test/functional_test.py
@@ -1,19 +1,31 @@
 import requests
 import json
+import unittest
 
-URL = "http://localhost:5000/map-recommend"
-response = requests.get(URL, data=json.dumps({"user": [{"latitude_0": 1, "longitude_0": 2},
-                                                       {"latitude_1": 3, "longitude_1": 4},
-                                                       {"latitude_2": 5, "longitude_2": 6}]}))
-success = response.json()["success"]
-print(response.json())
 
-URL = 'http://localhost:5000/user-recommend'
-user_id = 381
-response = requests.get(URL, data=json.dumps({"user_id": user_id}))
-print(response.json())
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.domain = 'http://localhost:5000/'
+    
+    def test_deeplearning(self):
+        URL = self.domain + "map-recommend"
+        response = requests.get(URL, data=json.dumps({"user": [{"latitude_0": 1, "longitude_0": 2},
+                                                            {"latitude_1": 3, "longitude_1": 4},
+                                                            {"latitude_2": 5, "longitude_2": 6}]}))
+        self.assertEqual(response.status_code, 200)
 
-URL = 'http://localhost:5000/location-recommend'
-location_id = 5315
-response = requests.get(URL, data=json.dumps({"location_id": location_id}))
-print(response.json())
+    def test_user_recommend(self):
+        URL = self.domain + "user-recommend"
+        user_id = 381
+        response = requests.get(URL, data=json.dumps({"user_id": user_id}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_location_recommend(self):
+        URL = self.domain + "location-recommend"
+        location_id = 5315
+        response = requests.get(URL, data=json.dumps({"location_id": location_id}))
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
